5566f35f160a73c3870cf36140abe53cbe8c83bb
RyoRim <taipham164@gmail.com>
2024-04-11 07:17:37 -0700

    Update UI / Optimize code

--- app/src/main/java/com/csc131/deltamedicalteam/ui/profile/ProfileUserFragment.java
+++ app/src/main/java/com/csc131/deltamedicalteam/ui/profile/ProfileUserFragment.java
@@ -1,7 +1,6 @@
 package com.csc131.deltamedicalteam.ui.profile;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +12,13 @@ import androidx.fragment.app.Fragment;
 
 import com.csc131.deltamedicalteam.R;
 import com.csc131.deltamedicalteam.model.User;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ProfileUserFragment extends Fragment {
-    private com.csc131.deltamedicalteam.databinding.FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        binding = com.csc131.deltamedicalteam.databinding.FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        return root;
+        com.csc131.deltamedicalteam.databinding.FragmentProfileBinding binding = com.csc131.deltamedicalteam.databinding.FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
